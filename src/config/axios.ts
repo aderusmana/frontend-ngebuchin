6c244f21e02078723c33038fb9da61564a71361e
aderusmana <aderusmana777@gmail.com>
2024-01-10 16:56:59 +0700

    add icon and eye in register form

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_API_URL, 
   timeout: 5000, 
   headers: {
     'Content-Type': 'application/json',
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { useMutation } from 'react-query';
-import * as apiClient from '../../api/apiClient';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../../api/apiClient";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export interface RegisterFormData {
   firstName: string;
@@ -13,21 +14,34 @@ export interface RegisterFormData {
 }
 
 const Register: React.FC = () => {
-  const { register,watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
 
-  const mutation = useMutation(apiClient.register ,{
+  const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
-      console.log('Registration successful');
+      console.log("Registration successful");
     },
     onError: (error) => {
-      console.error('Registration failed:', error);
+      console.error("Registration failed:", error);
     },
   });
 
   const onSubmit = (data: RegisterFormData) => {
     const { confirmPassword, ...registerData } = data;
     mutation.mutate(registerData);
-    console.log(data)
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   return (
@@ -35,78 +49,140 @@ const Register: React.FC = () => {
       <h2 className="text-4xl mb-4 text-left">Register</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <label htmlFor="firstName" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="firstName"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             First Name
           </label>
           <input
             type="text"
             id="firstName"
-            {...register('firstName', { required: "First Name is required" })}
+            {...register("firstName", { required: "First Name is required" })}
             placeholder="First Name"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
           />
-          {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
-
+          {errors.firstName && (
+            <p className="text-red-500">{errors.firstName.message}</p>
+          )}
         </div>
         <div className="mb-4">
-          <label htmlFor="lastName" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="lastName"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Last Name
           </label>
           <input
             type="text"
             id="lastName"
-            {...register('lastName', { required: "Last Name is required" })}
+            {...register("lastName", { required: "Last Name is required" })}
             placeholder="Last Name"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
           />
-          {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
+          {errors.lastName && (
+            <p className="text-red-500">{errors.lastName.message}</p>
+          )}
         </div>
         <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Email
           </label>
           <input
             type="email"
             id="email"
-            {...register('email', { required: "Email is required" })}
+            {...register("email", { required: "Email is required" })}
             placeholder="Email"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
           />
-          {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
         </div>
         <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            {...register('password', { required: "Password is required", minLength: { value: 5, message: "Password must be at least 5 characters" } })}
-            placeholder="Password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
-          />
-          {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 5,
+                  message: "Password must be at least 5 characters",
+                },
+              })}
+              placeholder="Password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
+            />
+            <span
+              className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <FaEyeSlash className="text-gray-500" />
+              ) : (
+                <FaEye className="text-gray-500" />
+              )}
+            </span>
+          </div>
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
         </div>
-        <div className="mb-6">
-          <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-bold mb-2">
+        <div className="mb-6 relative">
+          <label
+            htmlFor="confirmPassword"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Confirm Password
           </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            {...register('confirmPassword', { 
+          <div className="relative">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              id="confirmPassword"
+              {...register("confirmPassword", {
                 validate: (value) => {
-                    if(!value) {
-                        return "Confirm Password is required"
-                    }else if(watch('password') != value) {
-                        return "Password and Confirm Password must match"
-                    } }})}
-            placeholder="Confirm Password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
-          />
-          {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
+                  if (!value) {
+                    return "Confirm Password is required";
+                  } else if (watch("password") != value) {
+                    return "Password and Confirm Password must match";
+                  }
+                },
+              })}
+              placeholder="Confirm Password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
+            />
+
+            <span
+              className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+              onClick={toggleConfirmPasswordVisibility}
+            >
+              {showPassword ? (
+                <FaEyeSlash className="text-gray-500" />
+              ) : (
+                <FaEye className="text-gray-500" />
+              )}
+            </span>
+          </div>
+          {errors.confirmPassword && (
+            <p className="text-red-500">{errors.confirmPassword.message}</p>
+          )}
         </div>
-        <h3 className='pb-7 text-gray-500 text-sm'>Already have an account? <Link to="/login" className='hover:text-pink-800'>Login</Link></h3>
+        <h3 className="pb-7 text-gray-500 text-sm">
+          Already have an account?{" "}
+          <Link to="/login" className="hover:text-pink-800">
+            Login
+          </Link>
+        </h3>
         <button
           type="submit"
           className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
